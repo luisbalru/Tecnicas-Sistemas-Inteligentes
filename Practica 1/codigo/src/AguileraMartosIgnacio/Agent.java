0d0a36659aae85874535230df5bf88c6f997c7a4
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-02 13:45:59 +0100

    Esqueleto de la práctica 1

---
+++ Practica 1/codigo/src/AguileraMartosIgnacio/Agent.java
@@ -0,0 +1,22 @@
+package AguileraMartosIgnacio;
+
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+
+public class Agent extends AbstractPlayer{
+
+	public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		
+	}
+	
+	public void init(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		
+	}
+	
+	public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+		return Types.ACTIONS.ACTION_NIL;
+	}
+	
+}
---
+++ Practica 1/codigo/src/module-info.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+/**
+ * @author nacheteam
+ *
+ */
+module AguileraMartosIgnacio {
+	exports tracks.ruleGeneration.randomRuleGenerator;
+	exports tools.com.google.gson.internal.bind.util;
+	exports tracks.gameDesign;
+	exports tracks.levelGeneration.constructiveLevelGenerator;
+	exports tracks.multiPlayer.simple.sampleRandom;
+	exports ontology.sprites.npc;
+	exports tracks.singlePlayer;
+	exports tracks.singlePlayer.tools.replayer;
+	exports tracks.multiPlayer.advanced.sampleMCTS;
+	exports core.content;
+	exports tools.com.google.gson.stream;
+	exports tools.com.google.gson;
+	exports tracks.multiPlayer.deprecated.sampleGA;
+	exports ontology.effects.unary;
+	exports tracks.singleLearning;
+	exports tracks.multiPlayer.deprecated.sampleMCTS;
+	exports ontology;
+	exports tracks.singlePlayer.deprecated.sampleMCTS;
+	exports ontology.effects;
+	exports tracks.multiPlayer.simple.sampleOneStepLookAhead;
+	exports core.termination;
+	exports core.player;
+	exports tracks.singlePlayer.simple.sampleRandom;
+	exports testing;
+	exports tracks.singlePlayer.simple.doNothing;
+	exports tools.com.google.gson.internal;
+	exports tracks.multiPlayer.tools.human;
+	exports tracks.levelGeneration;
+	exports tracks.singleLearning.utils;
+	exports tracks.singlePlayer.tools.ucbOptimizerAgent;
+	exports tracks.singlePlayer.simple.sampleonesteplookahead;
+	exports tracks.levelGeneration.randomLevelGenerator;
+	exports core.optimization.ucbOptimization;
+	exports tracks.singlePlayer.advanced.sampleMCTS;
+	exports tracks.multiPlayer.tools.replayer;
+	exports tracks.levelGeneration.geneticLevelGenerator;
+	exports tracks.levelGeneration.constraints;
+	exports tracks.singlePlayer.simple.simpleRandom;
+	exports ontology.effects.binary;
+	exports tools.pathfinder;
+	exports tools.com.google.gson.internal.bind;
+	exports tracks.multiPlayer.advanced.sampleRHEA;
+	exports tracks.singlePlayer.deprecated.sampleGA;
+	exports tools;
+	exports tools.com.google.gson.reflect;
+	exports tracks.ruleGeneration.geneticRuleGenerator;
+	exports core.game;
+	exports core.vgdl;
+	exports core.competition;
+	exports tracks.multiPlayer.tools.heuristics;
+	exports tracks.multiPlayer;
+	exports tracks.multiPlayer.simple.doNothing;
+	exports ontology.sprites;
+	exports tracks.singlePlayer.advanced.olets;
+	exports tracks.ruleGeneration.constructiveRuleGenerator;
+	exports tracks.singlePlayer.tools.Heuristics;
+	exports tracks.multiPlayer.advanced.sampleRS;
+	exports ontology.avatar;
+	exports tracks.singlePlayer.tools.repeatOLETS;
+	exports tracks.singlePlayer.simple.greedyTreeSearch;
+	exports tools.com.google.gson.annotations;
+	exports tracks;
+	exports tracks.singlePlayer.advanced.sampleRS;
+	exports tracks.singlePlayer.tools.human;
+	exports tracks.ruleGeneration;
+	exports core.generator;
+	exports ontology.sprites.producer;
+	exports core.optimization;
+	exports ontology.physics;
+	exports ontology.avatar.oriented;
+	exports tracks.singlePlayer.advanced.sampleRHEA;
+	exports ontology.sprites.missile;
+	exports core.logging;
+
+	requires gson;
+	requires java.desktop;
+	requires java.management;
+	requires java.sql;
+}
